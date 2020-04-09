deaefec78f71e982864299a4c09a5ea99ba96541
Kevin Kirchner <kirchner.kevin@gmail.com>
2020-04-09 17:15:17 -0500

    better looking and better working grid

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="App">
+  <div class="App mt-4">
     <div class="container">
-      <div class="row">
-        <div class="col w-7">7</div>
-        <div class="col w-6">6</div>
-        <div class="col w-5">5</div>
-        <div class="col w-5">5</div>
-        <div class="col w-4">4</div>
-        <div class="col w-4">4</div>
-        <div class="col w-3">3</div>
-        <div class="col w-4">4</div>
-        <div class="col w-3">3</div>
-        <div class="col w-3">3</div>
-        <div class="col w-2">2</div>
-        <div class="col w-3">3</div>
-        <div class="col w-2">2</div>
-        <div class="col w-2">2</div>
-        <div class="col w-1">1</div>
+      <div class="row leading-5 text-gray-700">
+        <div class="col w-7 bg-gray-400 mb-2">.w-7</div>
+        <div class="col w-6 bg-gray-400 mb-2">.w-6</div>
+        <div class="col w-5 bg-gray-500 mb-2">.w-5</div>
+        <div class="col w-5 bg-gray-400 mb-2">.w-5</div>
+        <div class="col w-4 bg-gray-500 mb-2">.w-4</div>
+        <div class="col w-4 bg-gray-400 mb-2">.w-4</div>
+        <div class="col w-3 bg-gray-500 mb-2">.w-3</div>
+        <div class="col w-4 bg-gray-400 mb-2">.w-4</div>
+        <div class="col w-3 bg-gray-500 mb-2">.w-3</div>
+        <div class="col w-3 bg-gray-400 mb-2">.w-3</div>
+        <div class="col w-2 bg-gray-500 mb-2">.w-2</div>
+        <div class="col w-3 bg-gray-400 mb-2">.w-3</div>
+        <div class="col w-2 bg-gray-500 mb-2">.w-2</div>
+        <div class="col w-2 bg-gray-400 mb-2">.w-2</div>
+        <div class="col w-1 bg-gray-500 mb-2">.w-1</div>
       </div>
     </div>
   </div>
