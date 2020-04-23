5025a725445d7b18175ca88c60bd2f9545bbf8e2
Kevin Kirchner <kirchner.kevin@gmail.com>
2020-04-22 21:13:23 -0500

    vue add router; move content to Home.vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import router from './router'
 
 require('@/assets/scss/main.scss');
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+  router,
+  render: (h) => h(App)
 }).$mount('#app');
---
+++ src/views/Vert.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Vert flex">
+    <div class="border w-6 border-gray-500">6</div>
+    <div class="border w-5 border-gray-500 flex">
+      <div class="border w-6 border-gray-500">5</div>
+      <div class="border w-5 border-gray-500">5</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Vert',
+};
+</script>
+
+<style lang="scss" src="./Vert.scss"></style>
