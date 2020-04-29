9ca70319520ee3888c11f69d4ab8d6dab1e6e690
Kevin Kirchner <kirchner.kevin@gmail.com>
2020-04-29 13:30:30 -0500

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,35 @@
 let path = require('path');
 
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `$environment: '${process.env.NODE_ENV}';`,
-      },
-    },
-  },
+  // css: {
+  //   loaderOptions: {
+  //     sass: {
+  //       prependData: `$environment: '${process.env.NODE_ENV}';`,
+  //     },
+  //   },
+  // },
 
-  devServer: {
-    proxy: 'http://________SOME_URL_________.test/app',
-  },
+  // devServer: {
+  //   proxy: 'http://________SOME_URL_________.test/app',
+  // },
 
-  // output built static files to Laravel's public dir.
-  // note the "build" script in package.json needs to be modified as well.
-  outputDir: '../../../public/assets/app',
+  // // output built static files to Laravel's public dir.
+  // // note the "build" script in package.json needs to be modified as well.
+  // outputDir: '../../../public/assets/app',
 
-  publicPath: process.env.NODE_ENV === 'production' ? '/assets/app/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/assets/app/' : '/',
 
-  // modify the location of the generated HTML file.
-  indexPath:
-    process.env.NODE_ENV === 'production' ? '../../../resources/views/app.blade.php' : 'index.html',
+  // // modify the location of the generated HTML file.
+  // indexPath:
+  //   process.env.NODE_ENV === 'production' ? '../../../resources/views/app.blade.php' : 'index.html',
 
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@@': path.join(__dirname, '../../../'),
-      },
-    },
-  },
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       '@@': path.join(__dirname, '../../../'),
+  //     },
+  //   },
+  // },
 
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
